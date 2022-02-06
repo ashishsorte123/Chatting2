9db846cd9e970dbea990608c23c3493f4b3185ec
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-02-06 22:41:26 +0530

    emojis

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
+import {
+  View,
+  TextInput,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import {
   SimpleLineIcons,
@@ -10,9 +16,13 @@ import {
 import { Auth, DataStore } from "aws-amplify";
 import { Message } from "../../src/models";
 import { ChatRoom } from "../../src/models";
+import styles from "./styles";
+import EmojiSelector from "react-native-emoji-selector";
 
 const MessageInput = ({ chatRoom }) => {
   const [message, setMessage] = useState("");
+
+  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
 
   const sendMessage = async () => {
     const user = await Auth.currentAuthenticatedUser();
@@ -28,6 +38,8 @@ const MessageInput = ({ chatRoom }) => {
     updateLastMessage(newMessage);
 
     setMessage("");
+
+    setIsEmojiPickerOpen(false);
   };
 
   const updateLastMessage = async (newMessage) => {
@@ -51,79 +63,63 @@ const MessageInput = ({ chatRoom }) => {
   };
 
   return (
-    <View style={styles.root}>
-      <View style={styles.inputContainer}>
-        <SimpleLineIcons
-          name="emotsmile"
-          size={24}
-          color="#595959"
-          style={styles.icon}
-        />
-        <TextInput
-          style={styles.input}
-          value={message}
-          onChangeText={setMessage}
-          placeholder="Message..."
-        />
-        <Feather name="camera" size={24} color="#595959" style={styles.icon} />
-        <MaterialCommunityIcons
-          name="microphone-outline"
-          size={24}
-          color="#595959"
-          style={styles.icon}
-        />
+    <View style={[styles.root, { height: isEmojiPickerOpen ? "50%" : "auto" }]}>
+      <View style={styles.row}>
+        <View style={styles.inputContainer}>
+          <Pressable
+            onPress={() =>
+              setIsEmojiPickerOpen((currentValue) => !currentValue)
+            }
+          >
+            <SimpleLineIcons
+              name="emotsmile"
+              size={24}
+              color="#595959"
+              style={styles.icon}
+            />
+          </Pressable>
+
+          <TextInput
+            style={styles.input}
+            value={message}
+            onChangeText={setMessage}
+            placeholder="Message..."
+          />
+
+          <Feather
+            name="camera"
+            size={24}
+            color="#595959"
+            style={styles.icon}
+          />
+
+          <MaterialCommunityIcons
+            name="microphone-outline"
+            size={24}
+            color="#595959"
+            style={styles.icon}
+          />
+        </View>
+
+        <Pressable onPress={onPress} style={styles.buttonContainer}>
+          {message ? (
+            <Ionicons name="send" size={18} color="white" />
+          ) : (
+            <AntDesign name="plus" size={24} color="white" />
+          )}
+        </Pressable>
       </View>
-      <Pressable onPress={onPress} style={styles.buttonContainer}>
-        {message ? (
-          <Ionicons name="send" size={18} color="white" />
-        ) : (
-          <AntDesign name="plus" size={24} color="white" />
-        )}
-      </Pressable>
+
+      {isEmojiPickerOpen && (
+        <EmojiSelector
+          onEmojiSelected={(emoji) =>
+            setMessage((currentMessage) => currentMessage + emoji)
+          }
+          columns={8}
+        />
+      )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  root: {
-    flexDirection: "row",
-    padding: 10,
-  },
-
-  inputContainer: {
-    backgroundColor: "#f2f2f2",
-    flex: 1,
-    marginRight: 10,
-    borderRadius: 25,
-    borderWidth: 1,
-    borderColor: "#dedede",
-    alignItems: "center",
-    flexDirection: "row",
-    padding: 5,
-  },
-
-  input: {
-    flex: 1,
-    marginHorizontal: 5,
-  },
-
-  icon: {
-    marginHorizontal: 5,
-  },
-
-  buttonContainer: {
-    width: 40,
-    height: 40,
-    backgroundColor: "#3777f0",
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  buttonText: {
-    color: "white",
-    fontSize: 35,
-  },
-});
 
 export default MessageInput;
